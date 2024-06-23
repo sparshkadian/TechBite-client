4246e8247155df8ed84be13ccfa49a3a0a2004fd
Sparsh Kadian <sparshkadian323@gmail.com>
2024-06-23 22:22:39 +0530

    completed contact functionality

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,14 +1,46 @@
 import Button from '../components/Button';
+import { useState } from 'react';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
+
+  const { firstName, lastName, email, message } = formData;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevValue) => ({
+      ...prevValue,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+    const encodedMessage = encodeURIComponent(message);
+    const mailto = `mailto:sparshkadian323@gmail.com?body=${encodedMessage}`;
+    window.open(mailto, '_blank');
+  };
+
   return (
     <div>
-      <div className='max-w-4xl mx-auto px-5 mt-20 sm:mt-[100px]'>
+      <div className='max-w-4xl mx-auto px-5 mt-[50px] sm:mt-20'>
         <p className='text-center manrope-semibold text-2xl sm:text-4xl'>
           Drop Me a Line, Let Me Know What You Think
         </p>
 
-        <form className='flex flex-col gap-7 sm:gap-10 mt-10 sm:mt-20'>
+        <form
+          onSubmit={handleSubmit}
+          className='flex flex-col gap-7 sm:gap-10 mt-10 sm:mt-20'
+        >
           <div className='flex flex-col gap-7 sm:gap-0 sm:flex-row items-center justify-between'>
             <div className='relative flex flex-col gap-5 sm:w-[45%] w-full'>
               <label
@@ -18,6 +50,8 @@ const Contact = () => {
                 First Name
               </label>
               <input
+                value={firstName}
+                onChange={handleInputChange}
                 type='text'
                 id='firstName'
                 className='border-b-2 border-b-black focus:outline-none text-lg manrope-normal'
@@ -32,6 +66,8 @@ const Contact = () => {
                 Last Name
               </label>
               <input
+                value={lastName}
+                onChange={handleInputChange}
                 type='text'
                 id='lastName'
                 className='border-b-2 border-b-black focus:outline-none text-lg manrope-normal'
@@ -47,6 +83,8 @@ const Contact = () => {
               Email*
             </label>
             <input
+              value={email}
+              onChange={handleInputChange}
               type='email'
               id='email'
               className='border-b-2 border-b-black focus:outline-none text-lg manrope-normal w-full'
@@ -61,6 +99,8 @@ const Contact = () => {
               Message...
             </label>
             <input
+              value={message}
+              onChange={handleInputChange}
               type='text'
               id='message'
               className='border-b-2 border-b-black focus:outline-none text-lg manrope-normal w-full'
